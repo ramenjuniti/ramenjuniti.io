7f41708c6c0985eee326d0d7b383136979bc264a
ramenjuniti <ramenjuniti@gmail.com>
2018-12-23 23:01:17 +0900

    :tada: antd導入

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,10 +2,20 @@ module.exports = {
   siteMetadata: {
     name: `Hello Typescript World!`,
     tagline: `Gatsby + SASS + Typescript = 💪`
-  },  
+  },
   plugins: [
     `gatsby-plugin-sass`,
     `gatsby-plugin-typescript`,
     `gatsby-plugin-tslint`,
-  ],
-}
+    {
+      resolve: "gatsby-plugin-less",
+      options: {
+        javascriptEnabled: true,
+        modifyVars: {
+          "primary-color": "#FAAD13",
+          "layout-body-background": "#BEC8C8"
+        }
+      }
+    }
+  ]
+};
