c1eda68606cbe3cb6543f518408e38796c7d042f
ramenjuniti <ramenjuniti@gmail.com>
2018-12-29 00:10:12 +0900

    名前変更したので修正

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,47 +1,50 @@
 import { graphql } from "gatsby";
 import * as React from "react";
 
+import Layout from "../layouts";
+
 import About from "../components/About";
-import Layout from "../components/Layout";
 import Work from "../components/Work";
-import Works from "../components/Works";
+import WorkContent from "../components/WorkContent";
 
-import * as styles from "./Index.module.scss";
+import * as styles from "./index.module.scss";
 
-export interface Work {
+export interface WorkContentData {
   name: string;
   description: string;
   link?: string;
   github: string;
   youtube?: string;
+  tag: string[];
 }
 
 interface IndexProps {
   data: {
-    allWorksJson: {
-      edges: { node: Work }[];
+    allWorkJson: {
+      edges: { node: WorkContentData }[];
     };
   };
 }
 
 export default (props: IndexProps) => {
   // tslint:disable-next-line:no-console
-  console.log(props.data.allWorksJson);
+  console.log(props.data);
   return (
     <Layout>
       <div className={styles.Container}>
         <About />
-        <Works>
-          {props.data.allWorksJson.edges.map(value => (
-            <Work
+        <Work>
+          {props.data.allWorkJson.edges.map(value => (
+            <WorkContent
               name={value.node.name}
               description={value.node.description}
               link={value.node.link}
               github={value.node.github}
               youtube={value.node.youtube}
+              tag={value.node.tag}
             />
           ))}
-        </Works>
+        </Work>
       </div>
     </Layout>
   );
@@ -49,13 +52,33 @@ export default (props: IndexProps) => {
 
 export const indexQuery = graphql`
   query IndexQuery {
-    allWorksJson {
+    allAccountsJson {
+      edges {
+        node {
+          name
+          label
+          link
+        }
+      }
+    }
+    allCareerJson {
+      edges {
+        node {
+          name
+          term
+          post
+          description
+        }
+      }
+    }
+    allWorkJson {
       edges {
         node {
           name
           description
           link
           github
+          tag
         }
       }
     }
