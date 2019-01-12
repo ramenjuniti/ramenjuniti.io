87ef295735e3b764d83c69ba9d7ff68f9ee743c2
ramenjuniti <ramenjuniti@gmail.com>
2019-01-12 17:39:36 +0900

    :+1: icon出るようにした

--- src/components/Account/Account.tsx
+++ src/components/Account/Account.tsx
@@ -1,13 +1,18 @@
+import { Icon } from "antd";
 import * as React from "react";
 
 import { AccountData } from "../../pages/index";
 
 import styles from "./Account.module.scss";
 
+const IconFont = Icon.createFromIconfontCN({
+  scriptUrl: "//at.alicdn.com/t/font_986738_6tfptot7sa.js"
+});
+
 export default ({ name, label, link }: AccountData) => (
   <div className={styles.container}>
-    <h3>{name}</h3>
-    <p>{label}</p>
-    <p>{link}</p>
+    <a href={link} target="_blank" rel="noopener">
+      <IconFont type={`icon-${name}`} />
+    </a>
   </div>
 );
