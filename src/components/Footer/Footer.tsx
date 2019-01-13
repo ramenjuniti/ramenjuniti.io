153fdc708a734edf7d88411ce3413c12b8706496
ramenjuniti <ramenjuniti@gmail.com>
2019-01-14 02:30:11 +0900

    :lipstick: Headerいい感じになってきた

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -5,5 +5,14 @@ import styles from "./Footer.module.scss";
 export default () => (
   <footer className={styles.container}>
     <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
+    <h1>Footerだよ</h1>
   </footer>
 );
